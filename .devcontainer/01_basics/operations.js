e19f582c699a582ce2244c7243f0424500a3c076
rahil05mathur <87595242+rahil05mathur@users.noreply.github.com>
2024-01-09 18:45:02 +0000

    #

---
+++ .devcontainer/01_basics/operations.js
@@ -0,0 +1,43 @@
+let value = 3
+
+let negValue = -value;
+
+console.log(negValue);
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2) // modulus -> gives quotient
+console.log(2%3) // it gives reminder
+
+
+let str1 = "Akki"
+let str2 = "Mittal"
+
+let str3 = str1 + " " + str2;
+console.log(str3)
+
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+
+console.log(true) // ==> 1
+console.log(false) // ==> 0
+console.log(+true) // ==>1
+console.log(-false) // ==> -0
+console.log(+"") // +0
+
+// let num1, num2, num3;;
+// num1 = num2 = num3 = 2 + 2 // not considered
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter) // => 101
+--gameCounter;
+console.log(gameCounter) // => 100
+
+
