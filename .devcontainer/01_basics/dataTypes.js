72b5cfa4a05531c7485e39e4ef24321d1089c4c7
rahil05mathur <87595242+rahil05mathur@users.noreply.github.com>
2024-01-09 18:19:43 +0000

    #

---
+++ .devcontainer/01_basics/dataTypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all js code as a newer version
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined ==>
+// symbol ==> uniquw
+
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
