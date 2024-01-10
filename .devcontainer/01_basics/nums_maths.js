5d3ed94f3b0ef12b2d097dbb41adf46b9a12d185
rahil05mathur <87595242+rahil05mathur@users.noreply.github.com>
2024-01-10 15:56:21 +0000

    #

---
+++ .devcontainer/01_basics/nums_maths.js
@@ -0,0 +1,35 @@
+const score = 200
+
+const balance = new Number(100)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+
+
+const other_num = 123.3343
+
+
+console.log(other_num.toPrecision(3));
+
+const hundreds = 1000000
+
+console.log(hundreds.toLocaleString('en-IN'))
+// 10,00,000
+
+/*  MATHS Library  */
+
+console.log(Math.round(4.5)) // 5
+console.log(Math.round(4.4)) // 4
+console.log(Math.ceil(4.2)) // 5
+console.log(Math.floor(4.2)) // 4
+console.log(Math.sqrt(16)) // 4
+console.log(Math.min(4, 3, 6, 8)) // 3
+console.log(Math.max(10, 3, 6, 8)) // 10
+
+
+console.log(Math.random()); // b/w 0 and 1 but in decimals
+console.log((Math.random()*10) + 1);
+
+
+
+
