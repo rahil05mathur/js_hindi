8d0fb0e1cf5a847d417b5ef6c1eee4b9a272e78e
rahil05mathur <87595242+rahil05mathur@users.noreply.github.com>
2024-01-13 15:17:41 +0000

    #

---
+++ .devcontainer/01_basics/object_dstruct.js
@@ -0,0 +1,22 @@
+const course = {
+    courseName: "Js in Hindi",
+    price: "999",
+    Instructor: "Hitesh Choudhary"
+}
+
+const {Instructor} = course
+console.log(Instructor)
+
+
+// JSON
+
+// {
+//     "name": "Akshita Mittal",
+//     "age": 23,
+//     "city": "Jaipur",
+// }
+
+[
+    {},
+    {},
+]
